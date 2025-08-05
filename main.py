92150445a536ba017383111661900a546d1fbe10
HANG-XM <h2006z@163.com>
2025-08-05 15:13:20 +0800

    提高运行效率

--- main.py
+++ main.py
@@ -24,13 +24,10 @@ if __name__ == "__main__":
         # 初始化缓存目录
         initial()
 
-
         # 获取游戏窗口
         game_window_title = "BlueStacks 5"  # 请将此替换为你的游戏窗口的实际标题
         game_window = gw.getWindowsWithTitle(game_window_title)[0]
         game_window.activate()  # 激活游戏窗口
-        time.sleep(1)  # 等待窗口完全激活
-
         while running:
             # 查找聊天大厅
             position = find_template_on_screen(r"F:\Work\Know_Pic\click\Chat.png", game_window)
@@ -48,42 +45,39 @@ if __name__ == "__main__":
                     # 点击可抢
                     logging.info("点击可抢")
                     click_on_template(r"F:\Work\Know_Pic\click\Rob.png", game_window,position)
-                    time.sleep(2)  # 等待可抢完全加载
+                    time.sleep(1.8)  # 等待可抢完全加载
                     continue_click = True
                     while continue_click:
                         # 循环单个玩家的礼物
                         position = find_template_on_screen(r"F:\Work\Know_Pic\click\Treasure_chest.png",
                                                            game_window, 0.5)
-                        time.sleep(1)
                         if not position is None:
                             # 点击礼物
-                            time.sleep(2)  # 等待礼物完全加载
+                            time.sleep(1.5)  # 等待礼物完全加载
                             logging.info("点击礼物")
                             click_on_template(r"F:\Work\Know_Pic\click\Treasure_chest.png", game_window, position)
                             # 点击确认
                             time.sleep(1)  # 等待确认完全加载
                             logging.info("点击确认")
-                            if click_on_template(r"F:\Work\Know_Pic\click\Confirm.png", game_window):
+                            if not click_on_template(r"F:\Work\Know_Pic\click\Confirm.png", game_window):
                                 continue_click = False
                                 logging.info("今日礼物已经抢完！")
                         else:
                             continue_click = False
-                            logging.info("未找到可抢的礼物，开始返回主界面！")
+                            logging.info("该玩家已抢完礼物，开始返回主界面！")
                     # 点击返回
-                    time.sleep(1)  # 等待返回完全加载
                     logging.info("点击返回")
                     click_on_template(r"F:\Work\Know_Pic\click\Return.png", game_window)
+                    time.sleep(1)
                     # 返回主界面
-                    time.sleep(1)  # 等待返回完全加载
                     logging.info("返回主界面")
                     click_on_template(r"F:\Work\Know_Pic\click\Return_Main.png", game_window)
                 else:
-                    time.sleep(1)
                     logging.info("礼物大厅未找到可抢按钮，开始返回主界面！")
                     click_on_template(r"F:\Work\Know_Pic\click\Return_Main.png", game_window)
             else:
-                time.sleep(1)
-                logging.info("游戏窗口未找到聊天大厅！")
+                running=False
+                logging.info("游戏窗口未找到聊天大厅！结束程序！")
     except KeyboardInterrupt:
         logging.info("程序被手动中断，正在退出...")
     except Exception as e:
